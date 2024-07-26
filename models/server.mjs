fec1c902a7556ff0b469a08efed89007a2091bf9
kentayang <163032124+kentayang@users.noreply.github.com>
2024-07-26 18:17:05 -0500

    Default port updated

--- models/server.mjs
+++ models/server.mjs
@@ -5,7 +5,7 @@ import router from "../routes/users_route.mjs";
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.usersPath = "/api/users";
 
     //Middlewares
